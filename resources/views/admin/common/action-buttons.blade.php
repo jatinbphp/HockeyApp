0a015635d3df1a2e5cd2e6bb9ee8da6f80521168
jatinbphp <jatin.b.php@gmail.com>
2024-08-16 19:19:17 +0530

    action button chnages

--- resources/views/admin/common/action-buttons.blade.php
+++ resources/views/admin/common/action-buttons.blade.php
@@ -1,109 +1,23 @@
 
-@if($section_name=='users' || $section_name=='parent')
-
+@if($section_name!='children')
     <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
         <i class="fa fa-edit"></i>
     </a>
 
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
+    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url('admin/'.$section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
         <i class="fa fa-trash"></i>
     </button>
-
 @endif
-
 
 @if($section_name=='children')
 
     <a href="javascript:void(0)" data-toggle="tooltip" data-target="#childModel" data-url="{{ url('admin/'.$section_name.'/'.$id) }}" data-id="{{$id}}" data-original-title="Edit" class="edit btn btn-primary btn-sm editChild"><i class="fa fa-edit"></i></a>
 
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
+    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url('admin/'.$section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
         <i class="fa fa-trash"></i>
     </button>
 
 @endif
-
-@if($section_name=='category')
-
-    <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
-        <i class="fa fa-edit"></i>
-    </a>
-
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
-        <i class="fa fa-trash"></i>
-    </button>
-
-@endif
-
-
-@if($section_name=='province')
-
-    <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
-        <i class="fa fa-edit"></i>
-    </a>
-
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
-        <i class="fa fa-trash"></i>
-    </button>
-
-@endif
-
-@if($section_name=='school')
-
-    <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
-        <i class="fa fa-edit"></i>
-    </a>
-
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
-        <i class="fa fa-trash"></i>
-    </button>
-
-@endif
-
-
-@if($section_name=='skill')
-
-    <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
-        <i class="fa fa-edit"></i>
-    </a>
-
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
-        <i class="fa fa-trash"></i>
-    </button>
-
-@endif
-
-@if($section_name=='sponsors')
-
-    <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
-        <i class="fa fa-edit"></i>
-    </a>
-
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
-        <i class="fa fa-trash"></i>
-    </button>
-
-@endif
-
-@if($section_name=='email-templates')
-
-    <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
-        <i class="fa fa-edit"></i>
-    </a>
-
-    <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url($section_name.'/'.$id) }}" data-section="{{$section_name}}_table" title="Delete">
-        <i class="fa fa-trash"></i>
-    </button>
-
-@endif
-
-@if($section_name=='cms_page')
-
-    <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit" class="btn btn-sm btn-info tip">
-        <i class="fa fa-edit"></i>
-    </a>  
-
-@endif
-
 
 @if($section_name=='contact-us')
 
@@ -112,8 +26,6 @@
     </a> 
 
 @endif
-
-
 
 
 @if($section_name=='skill-review')
